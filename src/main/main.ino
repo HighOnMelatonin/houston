deff21e1295844e232072d479499a3232f88cb5b
HighOnMelatonin <tanruianh@outlook.com>
2025-02-22 06:35:29 +0800

    arduino ide nonsense

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,28 +1,19 @@
 // Main program
 #include <FastLED.h>
-#include "SoftwareSerial.h"
-#include "DFRobotDFPlayerMini.h"
 
 // Pin definitions
-static const uint8_t PIN_MP3_RX = 18
-static const uint8_t PIN_MP3_TX = 19
 #define buttonPin 20 // Pin for the button
-#define LED_PIN1 21 // Pin for the LED
-#define LED_PIN2 22 // Pin for the LED
-#define LED_PIN3 23 // Pin for the LED
-#define LED_PIN4 24 // Pin for the LED
-#define LED_PIN5 25 // Pin for the LED
-#define LED_PIN6 26 // Pin for the LED
-#define LED_PIN7 27 // Pin for the LED
-#define LED_PIN8 28 // Pin for the LED
+#define LED_PIN1 15 // Pin for the LED
+#define LED_PIN2 2 // Pin for the LED
+#define LED_PIN3 0 // Pin for the LED
+#define LED_PIN4 4 // Pin for the LED
+#define LED_PIN5 16 // Pin for the LED
+#define LED_PIN6 17 // Pin for the LED
+#define LED_PIN7 5 // Pin for the LED
+#define LED_PIN8 18 // Pin for the LED
 #define stepPin 3 // Pin for the stepper motor direction
-#define dirPin 2 // Pin for the stepper motor step
+#define dirPin 1 // Pin for the stepper motor step
 
-// Constants for the DFPlayer Mini
-#define moveTime 5000  // Duration of the audio
-SoftwareSerial mySoftwareSerial(PIN_MP3_RX, PIN_MP3_TX); // RX, TX
-// Create the DFPlayerMini object
-DFRobotDFPlayerMini player;
 
 // Constants for LED
 #define NUM_LEDS 18    // 18 LEDs per pin
@@ -35,26 +26,24 @@ CRGB leds[NUM_LEDS];
 
 // Constants for the stepper motor
 #define stepsPerRevolution 200
-#define revolutions 10
+#define revolutions 10  // Change this to change height of rocket
 
 // Constants for the RGB LED
 #define RGB_BRIGHTNESS 1
 #define RGB_BUILTIN 2
 
-#define BLINK_SPD
+#define BLINK_SPD 10
 
 /*
 Sequence of events
 1. Button pressed
 2. Send signal to LED strip to light up
 3. Send signal to stepper motor to move
-4. Send signal to DFPlayer Mini to play music
-5. Stop music (music will play for as long as the stepper motor is moving)
-6. Keep light on
-7. Keep moving
-8. Stop moving
-9. Stop light
-10. Rocket return to start
+4. Keep light on
+5. Keep moving
+6. Stop moving
+7. Stop light
+8. Rocket return to start
 */
 
 void setup() {
@@ -66,24 +55,15 @@ void setup() {
 
     // Init USB serial port for debugging
     Serial.begin(115200);
-    // Init serial port for DFPlayer Mini
-    mySoftwareSerial.begin(9600);
-    if (!player.begin(mySoftwareSerial)) {
-        Serial.println("Unable to begin");
-        return;
-    }
-    else {
-        Serial.println("DFPlayer Mini online");
-    }
 
-    FastLED.addLeds<WS2812, LED_PIN1, GRB>(leds, NUM_LEDS);
-    FastLED.addLeds<WS2812, LED_PIN2, GRB>(leds, NUM_LEDS);
-    FastLED.addLeds<WS2812, LED_PIN3, GRB>(leds, NUM_LEDS);
-    FastLED.addLeds<WS2812, LED_PIN4, GRB>(leds, NUM_LEDS);
-    FastLED.addLeds<WS2812, LED_PIN5, GRB>(leds, NUM_LEDS);
-    FastLED.addLeds<WS2812, LED_PIN6, GRB>(leds, NUM_LEDS);
-    FastLED.addLeds<WS2812, LED_PIN7, GRB>(leds, NUM_LEDS);
-    FastLED.addLeds<WS2812, LED_PIN8, GRB>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812, LED_PIN1>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812, LED_PIN2>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812, LED_PIN3>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812, LED_PIN4>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812, LED_PIN5>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812, LED_PIN6>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812, LED_PIN7>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812, LED_PIN8>(leds, NUM_LEDS);
 
     rgbLedWrite(RGB_BUILTIN, 0, 0, 0);
     delay(BLINK_SPD);
@@ -105,10 +85,7 @@ void loop(){
         lightUpLED();
         // Send signal to stepper motor to move
         moveStepperMotor();
-        // Send signal to DFPlayer Mini to play music
-        playMusic();
         // Stop moving
-        stopMoving();
         // Stop light
         stopLight();
         // Rocket return to start
@@ -121,6 +98,16 @@ void lightUpLED() {
     for (int i = 0; i < num_strips; i++) {
         for (int j = 0; j < NUM_LEDS; j++) {
             leds[j] = HSV;
+            FastLED.show();
+        }
+    }
+}
+
+void stopLight() {
+    Serial.println("Stopping light");
+    for (int i = 0; i < num_strips; i++) {
+        for (int j = 0; j < NUM_LEDS; j++) {
+            leds[j] = CRGB::Black;
             FastLED.show();
         }
     }
@@ -140,11 +127,14 @@ void moveStepperMotor() {
     }
 }
 
-void playMusic() {
-    Serial.println("Playing music");
-    mySoftwareSerial.begin(9600);
-
-    player.volume(20);
-    player.play(1);
-    delay(moveTime);
+void returnToStart() {
+    // Set the direction clockwise
+    digitalWrite(dirPin, HIGH);
+    // Step the motor
+    for (int i = 0; i < stepsPerRevolution * revolutions; i++) {
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(500);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(500);
+    }
 }
